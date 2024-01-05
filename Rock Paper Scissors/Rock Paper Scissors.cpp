f51f1fa505efde599340b94a89c07c850ded36b2
Tuana <57767763+tuananohut@users.noreply.github.com>
2024-01-05 17:39:43 +0300

    Add files via upload

---
+++ Rock Paper Scissors/Rock Paper Scissors.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <random>
+#include <vector>
+
+std::vector<std::string> moves = { "rock", "paper", "scissors" };
+
+struct Player
+{
+	int moveIndex;
+	std::string move;
+};
+
+Player player1 = Player();
+Player player2 = Player();
+
+int generateRandomNumber(float min, float max)
+{
+	static std::random_device rd;
+	static std::mt19937 gen(rd());
+	std::uniform_real_distribution<float> dis(min, max);
+	return dis(gen);
+}
+
+bool win();
+
+int main()
+{
+	std::cout << "Rock: 0" << std::endl;
+	std::cout << "Paper: 1" << std::endl;
+	std::cout << "Scissors: 2" << std::endl;
+	std::cout << "------------------------------" << std::endl;
+	player1.moveIndex = -1;
+	player2.moveIndex = -1;
+	while (!win())
+	{
+		std::cout << "Player 1: ";
+		std::cin >> player1.moveIndex;
+		player1.move = moves[player1.moveIndex];
+		player2.moveIndex = generateRandomNumber(0, 2);
+		player2.move = moves[player2.moveIndex];
+		std::cout << "Player 1 chose: " << player1.move << std::endl;
+		std::cout << "Player 2 chose: " << player2.move << std::endl;
+		std::cout << "------------------------------" << std::endl;
+	}
+	return 0;
+}
+bool win()
+{
+	if ((player1.moveIndex == 0 && player2.moveIndex == 2) ||
+		(player1.moveIndex == 1 && player2.moveIndex == 0) ||
+		(player1.moveIndex == 2 && player2.moveIndex == 1))
+	{
+		std::cout << "Player 1 wins!" << std::endl;
+		return true;
+	}
+	else if ((player2.moveIndex == 0 && player1.moveIndex == 2) ||
+		(player2.moveIndex == 1 && player1.moveIndex == 0) ||
+		(player2.moveIndex == 2 && player1.moveIndex == 1))
+	{
+		std::cout << "Player 2 wins!" << std::endl;
+		return true;
+	}
+	return false;
+}
