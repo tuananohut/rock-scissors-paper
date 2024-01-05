1d3dc1a95241c0b29433af71fd4f105b11ddb66d
Tuana <57767763+tuananohut@users.noreply.github.com>
2024-01-05 20:07:12 +0300

    Add files via upload

--- Rock Paper Scissors/Rock Paper Scissors.cpp
+++ Rock Paper Scissors/Rock Paper Scissors.cpp
@@ -11,7 +11,7 @@ struct Player
 	std::string move;
 };
 
-Player player1 = Player();
+Player player1 = Player();	
 Player player2 = Player();
 
 int generateRandomNumber(float min, float max)
@@ -26,6 +26,7 @@ bool win();
 
 int main()
 {
+	// When you play rock-paper-scissors, you should one of them. These are the number definitions. If you want to choose rock, you have to enter 0.
 	std::cout << "Rock: 0" << std::endl;
 	std::cout << "Paper: 1" << std::endl;
 	std::cout << "Scissors: 2" << std::endl;
@@ -37,7 +38,7 @@ int main()
 		std::cout << "Player 1: ";
 		std::cin >> player1.moveIndex;
 		player1.move = moves[player1.moveIndex];
-		player2.moveIndex = generateRandomNumber(0, 2);
+		player2.moveIndex = generateRandomNumber(0, 2); // Computer chooses random number.
 		player2.move = moves[player2.moveIndex];
 		std::cout << "Player 1 chose: " << player1.move << std::endl;
 		std::cout << "Player 2 chose: " << player2.move << std::endl;
@@ -47,7 +48,8 @@ int main()
 }
 bool win()
 {
-	if ((player1.moveIndex == 0 && player2.moveIndex == 2) ||
+	// Winning strategy 
+	if ((player1.moveIndex == 0 && player2.moveIndex == 2) ||   
 		(player1.moveIndex == 1 && player2.moveIndex == 0) ||
 		(player1.moveIndex == 2 && player2.moveIndex == 1))
 	{
